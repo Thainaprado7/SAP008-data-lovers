286f3611bb1ce81a2edb5cf5e61089bf28143a0c
Thainaprado7 <thaina.prado.psic@gmail.com>
2022-10-20 11:58:21 -0300

    movies att

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,41 @@
 import data from "./data/ghibli/ghibli.js"
 import { order } from "./data.js";
-import { printaCards, montaCard  } from "./main";
+
+function PrintandoCard(film) {
+    return `
+        <article>
+            <p> ${film.title}</p>
+            <img id= "imagem1" src="${film.poster}"/></p>
+            <p>Director: ${film.director}</p>
+            <p>Release date: ${film.release_date}</p>
+        </article>
+        `;
+}
+
+function montaCard(lista) {
+    let directorFilter = document.getElementById("filterDirector").value;
+    let yearFilter = document.getElementById("filterYear").value;  
+    let searchMovies = document.getElementById("searchMovie").value;
+
+    if (directorFilter){
+        lista =  lista.filter((obj) => obj.director == directorFilter);
+    }
+
+    if (yearFilter){
+        lista = lista.filter((obj) => obj.release_date == yearFilter);
+    }
+
+    if (searchMovies){
+        lista = lista.filter((obj) => obj.title.toLowerCase().includes(searchMovies.toLowerCase()))
+    }
+
+
+    return lista
+        .map((film) => {
+            return PrintandoCard(film);
+        })
+        .join("");
+}
 
 
 printaCards.innerHTML = montaCard(data.films);
